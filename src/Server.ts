30d0567a032f331b864adb54983e529a45fafe25
Loopilla <loopilla@gmail.com>
2019-08-09 14:40:01 +0200

    up and running

---
+++ src/Server.ts
@@ -0,0 +1,28 @@
+import * as fastify from 'fastify';
+import * as http from 'http';
+
+export class Server {
+    private _instance: fastify.FastifyInstance<http.Server, http.IncomingMessage, http.ServerResponse>;
+
+    constructor() {
+        console.log('constructor2');
+        this.init();
+        this.start()
+    }
+
+    private init() {
+        this._instance = fastify();
+
+        this._instance.get('/status',  function (request: fastify.FastifyRequest, reply: fastify.FastifyReply<http.ServerResponse>) {
+            reply.send({msg: 'Running'});
+        });
+    }
+
+    private start() {
+        this._instance.listen(3000).then(() => {
+            console.log('Server listening on 3000.');
+        });
+    }
+}
+
+module.exports = new Server();
